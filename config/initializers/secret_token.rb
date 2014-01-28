4d8aac44834fdb5e99be566be8883e313d9e289d
Jeremy Rottman <rottman@gmail.com>
2014-01-28 01:32:48 +0000

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MangaBytes::Application.config.secret_key_base = '9238ca7d0990f09bd0fb6f39927086682e242fe2012a722654546c1610e7b850c05d98271689b1d73871042fa121e3931022d250451f70d01a00103427226fa0'
