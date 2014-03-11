354f1a2d76174d9b8736afe5637b724a92f0f177
evanniedojadlo <evanniedojadlo@gmail.com>
2014-03-11 00:50:18 -0500

    Updated the Mangas view to include the seeded Manga images

--- lib/seeds/manga_seeds.rb
+++ lib/seeds/manga_seeds.rb
@@ -1,22 +1,12 @@
+require 'csv'
 class MangaSeeds 
-	IMPORTS_MANGAS = %w{ 
-
-".hack//sign",
-"Bleach"
-"Dragon Ball",
-"Hunter X Hunter", 
-"Naruto",
-"One Piece", 
-"Karate Shoukoushi Kohinata Minoru",
-"Ragnarok",
-"Ranma 1/2","Sword Art Online",
-
-}
 
 	def self.seed_mangas
-		IMPORTS_MANGAS.each do |manga|
-			Manga.delete_all
-			Manga.create(title: manga, image: File.open("#{Rails.root}/spec/files/cute-kitten-playing.jpg"))
+		Manga.destroy_all
+		file = "#{Rails.root}/spec/files/csv/mb_data.csv"
+		CSV.foreach(file, headers: true) do |row|
+			image = "#{Rails.root}/spec/files/images/#{row['Image']}"
+			Manga.create(title: row['Titles'], image: File.open(image))
 		end
 	end
 
