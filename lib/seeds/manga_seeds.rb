6d3aa39cb6bc67767f1c02240151883424f0e90b
evanniedojadlo <evanniedojadlo@gmail.com>
2014-03-14 01:09:27 -0500

    Updated the manga partial with some bootstrap elements to try out, added the tire gem to gemfiles.

--- lib/seeds/manga_seeds.rb
+++ lib/seeds/manga_seeds.rb
@@ -4,7 +4,7 @@ class MangaSeeds
 	def self.seed_mangas
 		Manga.destroy_all
 		file = "#{Rails.root}/spec/files/csv/mb_data.csv"
-		CSV.foreach(file, headers: true) do |row|
+		CSV.foreach(file, headers: true) do |row| 
 			image = "#{Rails.root}/spec/files/images/#{row['Image']}"
 			Manga.create(title: row['Titles'], image: File.open(image))
 		end
