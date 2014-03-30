94d42a3c0aca93bd72fb814c4b815157ec252414
Evan Niedojadlo <evanniedojadlo@gmail.com>
2014-03-29 19:08:09 -0500

    Added an html and text email template and updated the requests email class. The templates will need to be updated.

--- app/mailers/requests_email.rb
+++ app/mailers/requests_email.rb
@@ -1,3 +1,9 @@
 class RequestsEmail < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "hello@mangabytes.com"
+
+  def welcome_email(user)
+  	@user = user
+  	@url = 'http://example.com/login'
+  	mail(to: @user.email, subject: 'Welcome to Mangabytes!')
+  end
 end
