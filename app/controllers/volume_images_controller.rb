d41e371b8286215a0760f87276c0690171d6a099
Evan Niedojadlo <evanniedojadlo@gmail.com>
2014-03-31 02:01:18 -0500

    Created the Volume Images Controller and began modifying the shows view.

--- app/controllers/volume_images_controller.rb
+++ app/controllers/volume_images_controller.rb
@@ -2,10 +2,10 @@ class VolumesController < ApplicationController
 	before_filter :authenticate_user!
 
   def index
-    @volumes = Volume.order(:volume_ids)
+    @volume_images = Images.order(:volume_images)
   end
 
   def show
-    @volumes = Volume.find(params[:id])
+    @volume_image = Images.find(params[:id])
   end
 end
