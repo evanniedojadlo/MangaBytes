518331ddba0aece9cb60b9062320a8fd0e886b55
evanniedojadlo <evanniedojadlo@gmail.com>
2014-04-01 13:35:48 -0500

    Updated the Manga model in preparation for the search field update

--- app/models/manga.rb
+++ app/models/manga.rb
@@ -9,4 +9,9 @@ class Manga < ActiveRecord::Base
   include Tire::Model::Search
   include Tire::Model::Callbacks
 
+  def self.search(params)
+  	tire.search(load: true) do
+  		query { string params[:query] } if params[:query].present?
+  	end
+  end
 end
